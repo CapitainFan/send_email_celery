9bc63bf646b233fa4cb064de9d86d5366164e46d
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2024-03-24 21:29:18 +0300

    added schedule sending email and connected admin page

--- send_email/send_email/settings.py
+++ send_email/send_email/settings.py
@@ -128,10 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'xxxxxxx'
-# EMAIL_HOST_PASSWORD = 'xxxxxxxx'
-EMAIL_HOST_USER = 'bossbog18@gmail.com'
-EMAIL_HOST_PASSWORD = 'osgh pksb rcxu njpx'
+EMAIL_HOST_USER = 'xxxxxxx'
+EMAIL_HOST_PASSWORD = 'xxxxxxxx'
+
 EMAIL_PORT = 587
 
 REDIS_HOST = '0.0.0.0'
