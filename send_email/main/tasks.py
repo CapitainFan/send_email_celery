95ab17e270371dd14a5d903e2d80aaf2161afb8c
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2024-03-25 15:23:24 +0300

    took a practice with flower

--- send_email/main/tasks.py
+++ send_email/main/tasks.py
@@ -1,6 +1,6 @@
 from django.core.mail import send_mail
 from send_email.celery import app
-
+from celery import shared_task
 from .service import send
 from .models import Contact
 
@@ -20,3 +20,30 @@ def send_beat_email():
             [contact.email],
             fail_silently=False,
         )
+
+
+@app.task()
+def test_task_1():
+    print('Worked')
+    return True
+
+
+@app.task()
+def test_task_2(a, b):
+    c = a + b
+    return c
+# test_task_2.apply_async((5, 5), link=test_task_2.s(20))
+
+
+@app.task(bind=True, default_retry_delay=5*60)
+def test_task_3(self, x, y):
+    try:
+        return x + y
+    except Exception as exc:
+        raise self.retry(exc=exc, countdown=60)
+# test_task_3.apply_async(countdown=60)
+
+
+@shared_task()
+def test_sh_task(msg):
+    return msg + '!!!'
